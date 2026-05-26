c07617a5128ffd992b542a41c1dea574864a3046
Akhilesh Singh <akhileshs68@gmail.com>
2026-05-26 10:20:43 +0530

    restore full home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import Feed from "../components/feed"
 import BottomNav from "../components/bottom-nav"
+import CreatePost from "../components/create-post"
+import Header from "../components/header"
 
 export default function Home() {
 
@@ -8,13 +12,105 @@ export default function Home() {
     <main
       className="
         min-h-screen
-        bg-black
+        bg-[#020617]
         text-white
+        flex
+        flex-col
+        items-center
         pb-28
       "
     >
 
-      <Feed />
+      {/* HEADER */}
+      <Header />
+
+      {/* STORIES */}
+
+      <div
+        className="
+          w-full
+          max-w-[520px]
+          flex
+          gap-5
+          overflow-x-auto
+          px-4
+          py-5
+        "
+      >
+
+        {[
+          "You",
+          "Pi",
+          "Tech",
+          "Crypto"
+        ].map((item, i) => (
+
+          <div
+            key={i}
+            className="
+              flex
+              flex-col
+              items-center
+              text-white
+            "
+          >
+
+            <img
+              src={`https://i.pravatar.cc/150?img=${i + 10}`}
+              className="
+                w-16
+                h-16
+                rounded-full
+                border-4
+                border-pink-500
+              "
+            />
+
+            <span
+              className="
+                text-sm
+                mt-2
+              "
+            >
+              {item}
+            </span>
+
+          </div>
+
+        ))}
+
+      </div>
+
+      {/* CREATE POST */}
+
+      <div
+        className="
+          w-full
+          max-w-[520px]
+          px-4
+        "
+      >
+
+        <CreatePost />
+
+      </div>
+
+      {/* FEED */}
+
+      <div
+        className="
+          w-full
+          max-w-[520px]
+          px-4
+          mt-6
+        "
+      >
+
+        <Feed />
+
+      </div>
+
+      {/* NAVBAR */}
 
       <BottomNav />
 
